0fee5716e6a769ea291d43f35d06c133976a3f1b
TheRealFakeAdmin <TheRealFakeAdmin@pm.me>
2025-07-06 01:12:10 -0400

    converted App.jsx fragment to div
    - Changed App.css to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
     const name = "World";
 
     return (
-        <>
+        <div className="main">
             <h1>Hello, {name}!</h1>
-        </>
+        </div>
     );
 }
 
