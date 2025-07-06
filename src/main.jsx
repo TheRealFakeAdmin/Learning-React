e3570f8762b980e82b696a125bc123aa855da7e2
TheRealFakeAdmin <TheRealFakeAdmin@pm.me>
2025-07-06 02:30:21 -0400

    Added Header and Footer
    - Changed to flex layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
+import "./style.scss";
+
+import Header from "./components/header/Header.jsx"
 import App from "./App.jsx";
+import Footer from "./components/footer/Footer.jsx"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <Header title="Learning React" />
         <App />
+        <Footer text="&copy; 2025 Learning React" />
     </StrictMode>
 );
